55e0581d70792e2e44cf6c4409c433b54d49f9df
teddywests1de <dirkieys21@gmail.com>
2024-10-14 15:51:31 +0200

    Adding Form State

--- src/RSVP.js
+++ src/RSVP.js
@@ -1,5 +1,25 @@
+import { useState } from "react";
 export function RSVP() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [attend, setAttend] = useState(false);
 
+    function handleRadioChange(value) {
+        setAttend(value);
+        console.log(value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const response = {
+            firstName,
+            lastName,
+            email,
+            attend,
+        }
+        console.log(response);
+    }
     return(
         <div className="rsvp">
             <div className="title"><h3>RSVP</h3></div>
@@ -8,24 +28,24 @@ export function RSVP() {
                 <p>RSVP by January 2025</p>
             </div>
             <div>
-                <form className="rsvp-form">
+                <form className="rsvp-form" onSubmit={handleSubmit}>
                     <div className="form-row">
                         <div className="form-field"><label>Name (required)</label></div>
                         <div className="form-field">
-                            <input type="text" name="first-name" placeholder="John"/>
-                            <input type="text" name="last-name" placeholder="Smith"/>
+                            <input type="text" name="first-name" placeholder="John" value={firstName} onChange={(e)=>setFirstName(e.target.value)}/>
+                            <input type="text" name="last-name" placeholder="Smith" value={lastName} onChange={(e)=>setLastName(e.target.value)}/>
                         </div>
                     </div>
                     <div className="form-row">
                         <div className="form-field"><label>Email (required)</label></div>
                         <div className="form-field">
-                            <input type="email" name="email" placeholder="john.smith@gmail.com"/>
+                            <input type="email" name="email" placeholder="john.smith@gmail.com" value={email} onChange={(e)=>setEmail(e.target.value)}/>
                         </div>
                     </div>
                     <p>Will you be attending? (Required)</p>
                     <div className="form-row">
-                        <div className="form-field"><label for="yes"> <input type="radio" id="yes" name="attending" value="yes" />Yes, I'll be there!</label></div>
-                        <div className="form-field"><label for="no"> <input type="radio" id="no" name="attending" value="no" />No, I won't be able to attend.</label></div>
+                        <div className="form-field"><label for="yes"> <input type="radio" id="yes" name="attending" value={attend} onChange={()=>handleRadioChange("Yes")} />Yes, I'll be there!</label></div>
+                        <div className="form-field"><label for="no"> <input type="radio" id="no" name="attending" value={attend} onChange={()=>handleRadioChange("No")}/>No, I won't be able to attend.</label></div>
                     </div>
                     <div className="form-row">
                         <button type="submit" className="btn">Submit</button>
